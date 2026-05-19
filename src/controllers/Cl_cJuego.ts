926534fdcddb891046411b121d6c3ca9a25a2ef0
barbara3108 <barbaravielma3.0@gmail.com>
2026-05-19 11:32:23 -0400

    LoQueUstedQuiera

---
+++ src/controllers/Cl_cJuego.ts
@@ -0,0 +1,15 @@
+/*         JUEGO DE CARTAS
+
+        En el juego a cada participante se le entregan 3 cartas numeradas del 1 al 12, 
+    gana quien sume mayor puntuación.
+
+        Se desea conocer a) la puntuación de cada participante, b) el nombre del ganador, 
+    c) el porcentaje de jugadores con menos de 10 puntos.
+
+    Se tiene que el formato para la salida de los requerimientos solicitados es:
+    El participante Juan tiene una puntuación de 8
+    El participante José tiene una puntuación de 14
+    El participante Rosa tiene una puntuación de 27
+    Nombre del ganador: Rosa
+    Porcentaje de jugadores con menos de 10 puntos:33.33% 
+*/
